5c12536a5ddbb598eb60e736229c5740f66f3aab
kailas-lab <kailashraina9@gmail.com>
2025-09-24 16:37:19 +0530

    version4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2 group">
             <img
-              src="public\name.webp"
+              src="public\name.png"
               alt="Future Key Logo"
               className="h-12 w-auto group-hover:scale-110 transition-transform duration-300"
             />
