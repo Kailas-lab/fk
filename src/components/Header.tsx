d0562880dfdaa95d2b85eac0757f15541e843709
kailas-lab <kailashraina9@gmail.com>
2025-09-24 16:35:00 +0530

    version4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import Name from '../../public/name.webp';
+
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
