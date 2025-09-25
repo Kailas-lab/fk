28f8cc9da11dcb74bb1f00a3d980a1d92b56bb8d
kailas-lab <kailashraina9@gmail.com>
2025-09-25 12:44:54 +0530

    version3

--- src/pages/PythonCourse.tsx
+++ src/pages/PythonCourse.tsx
@@ -181,12 +181,7 @@ const PythonCourse = () => {
             {/* Content */}
             <div className="text-center lg:text-left">
               <div className="inline-flex items-center px-4 py-2 bg-emerald-900/50 rounded-full text-emerald-400 text-sm font-medium mb-6 backdrop-blur-sm border border-emerald-800">
-                <img
-                  // CHANGE THIS LINE
-                  src={logo} // Use the imported 'logo' variable here
-                  alt="Future Key"
-                  className="h-4 w-auto mr-2"
-                />
+                
                 Python Programming Fundamentals
               </div>
 
